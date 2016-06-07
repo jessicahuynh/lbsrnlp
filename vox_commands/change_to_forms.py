ecf7fcdb36a327c55f1cfbafee9107375d539683
Jessica Huynh <jhuynh37@brandeis.edu>
2016-06-07 17:30:14 -0400

    create functional forms

--- vox_commands/change_to_forms.py
+++ vox_commands/change_to_forms.py
@@ -2,8 +2,37 @@ import nltk
 
 forms = open('functional_forms.txt','w')
 
+events = list(open('events.txt'))
+events = [x.strip() for x in events]
+
+objects = list(open('objects.txt'))
+objects = [x.strip() for x in objects]
+
+relations = list(open('relations.txt'))
+relations = [x.strip() for x in relations]
+relations.append('against')
+
 with open('sentences.txt') as f:
 	for sentence in f:
-		print(s)
+		s = sentence.split()
+		form = s[0] + '('
+		for i in range(1,len(s)):
+			if s[i] in relations:
+				form = form + ',' + s[i] + '('
+			elif (s[i] == 'in' and s[i+1] == 'front' and s[i+2] == 'of'):
+				form = form + ',in_front_of('
+			elif s[i] == 'left' and s[i+1] == 'of':
+				form = form + ',left_of('
+			elif (s[i] == 'right' and s[i+1] == 'of'):
+				form = form + ',right_of('
+			elif s[i] in objects:
+				form = form + s[i]
+			elif s[i] == 'paper' and s[i+1] == 'sheet':
+				form = form + 'paper_sheet'
+
+		form = form + ')'
+		if ',' in form:
+			form = form + ')'
+		print(form,file=forms)
 
 forms.close()
