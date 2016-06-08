552a40cdb221d3fced084863579a6620d373f1df
Jessica Huynh <jhuynh37@brandeis.edu>
2016-06-07 20:47:07 -0400

    able to pass one sentence as an argument

--- vox_commands/change_to_forms.py
+++ vox_commands/change_to_forms.py
@@ -1,6 +1,29 @@
-import nltk
+import sys
 
-forms = open('functional_forms.txt','w')
+def parse_sent(s):
+	form = s[0] + '('
+	for i in range(1,len(s)):
+		if s[i] in relations:
+			form = form + ',' + s[i] + '('
+		elif (s[i] == 'in' and s[i+1] == 'front' and s[i+2] == 'of'):
+			form = form + ',in_front_of('
+		elif s[i] == 'left' and s[i+1] == 'of':
+			form = form + ',left_of('
+		elif (s[i] == 'right' and s[i+1] == 'of'):
+			form = form + ',right_of('
+		elif s[i] in objects:
+			form = form + s[i]
+		elif s[i] == 'paper' and s[i+1] == 'sheet':
+			form = form + 'paper_sheet'
+		elif s[i] == 'edge':
+			form = form + 'edge'
+		elif s[i] == 'center':
+			form = form + 'center'
+
+	form = form + ')'
+	if ',' in form:
+		form = form + ')'
+	return form
 
 events = list(open('events.txt'))
 events = [x.strip() for x in events]
@@ -13,31 +36,15 @@ relations = [x.strip() for x in relations]
 relations.append('against')
 relations.append('at')
 
-with open('sentences.txt') as f:
-	for sentence in f:
-		s = sentence.split()
-		form = s[0] + '('
-		for i in range(1,len(s)):
-			if s[i] in relations:
-				form = form + ',' + s[i] + '('
-			elif (s[i] == 'in' and s[i+1] == 'front' and s[i+2] == 'of'):
-				form = form + ',in_front_of('
-			elif s[i] == 'left' and s[i+1] == 'of':
-				form = form + ',left_of('
-			elif (s[i] == 'right' and s[i+1] == 'of'):
-				form = form + ',right_of('
-			elif s[i] in objects:
-				form = form + s[i]
-			elif s[i] == 'paper' and s[i+1] == 'sheet':
-				form = form + 'paper_sheet'
-			elif s[i] == 'edge':
-				form = form + 'edge'
-			elif s[i] == 'center':
-				form = form + 'center'
+if len(sys.argv) == 2:
+	print(parse_sent(sys.argv[1].split()))
+else:
+	forms = open('functional_forms.txt','w')
 
-		form = form + ')'
-		if ',' in form:
-			form = form + ')'
-		print(form,file=forms)
+	with open('sentences.txt') as f:
+		for sentence in f:
+			s = sentence.split()
+			
+			print(parse_sent(s),file=forms)
 
-forms.close()
+	forms.close()
